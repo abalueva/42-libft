39c23aad1f03d83d9d390d3ead26c0617450089b
abalueva <evaandbalu@gmail.com>
2019-10-03 14:34:32 -0700

    strsplit again

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,99 +6,72 @@
 /*   By: abalueva <abalueva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 13:00:53 by vtouffet          #+#    #+#             */
-/*   Updated: 2019/10/03 14:29:57 by abalueva         ###   ########.fr       */
+/*   Updated: 2019/10/03 14:34:25 by abalueva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static int		is_separator(char c, char const *charset)
+#include <stdlib.h>
+#include "libft.h"
+
+static int		ft_find_size(char const *s, char c)
 {
-	int	i;
-
-	i = 0;
-	while (charset[i])
-	{
-		if (c == charset[i])
-			return (1);
-		++i;
-	}
-	return (0);
-}
-
-static int		count_words(char *str, const char *charset)
-{
-	int	count;
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (str[i] && is_separator(str[i], charset))
-		++i;
-	if (str[i] && !is_separator(str[i], charset))
-		count = 1;
-	while (str[i])
-	{
-		if (!is_separator(str[i], charset) && is_separator(str[i - 1], charset))
-			++count;
-		++i;
-	}
-	return (count);
-}
-
-static int		word_size(char *str, char const *charset)
-{
-	int	i;
-	int	size;
+	int		i;
+	int		size;
 
 	i = 0;
 	size = 0;
-	while (str[i] && !is_separator(str[i], charset))
+	while (s[i] != '\0')
 	{
-		++size;
-		++i;
+		while (s[i] == c)
+			i++;
+		size++;
+		while (s[i] != c)
+			i++;
+		i++;
 	}
 	return (size);
 }
 
-char			**ft_strsplitchrset(char *str, const char *charset)
+static char		**find(char const *s, char c, int len)
 {
-	char	**tab;
 	int		i;
 	int		j;
-	int		index;
+	int		z;
+	char	**fresh;
 
-	if (!(tab = (char**)malloc(sizeof(char*) *
-					(count_words(str, charset) + 1))))
+	i = 0;
+	z = 0;
+	if (!(fresh = malloc(sizeof(char *) * (len + 1))))
 		return (NULL);
-	i = -1;
-	index = -1;
-	while (str[++i])
-		if (!is_separator(str[i], charset))
-		{
-			if (index == -1 || is_separator(str[i - 1], charset))
-			{
-				if (!(tab[++index] = (char*)malloc(sizeof(char) * (word_size(
-						str + i, charset) + 1))))
-					return (NULL);
-				j = 0;
-			}
-			tab[index][j] = str[i];
-			tab[index][++j] = '\0';
-		}
-	tab[++index] = 0;
-	return (tab);
+	while (s[i] != '\0')
+	{
+		while (s[i] == c && s[i] != '\0')
+			++i;
+		j = i;
+		while (s[j] != c && s[j] != '\0')
+			++j;
+		fresh[z] = ft_strsub(s, i, j - i);
+		if (fresh[z][0] == '\0')
+			return (NULL);
+		z++;
+		i = j;
+		while (s[i] == c && s[i] != '\0')
+			++i;
+	}
+	return (fresh);
 }
 
-char		**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(char const *s, char c)
 {
-	char	*charset;
+	int		len;
+	char	**fresh;
 
-	if (!s || !c)
+	if (!s)
 		return (NULL);
-	if (!(charset = ft_strnew(2)))
-		return (NULL);
-	charset[0] = c;
-	return (ft_strsplitchrset((char*)s, charset));
+	len = ft_find_size(s, c);
+	fresh = find(s, c, len);
+	return (fresh);
 }
