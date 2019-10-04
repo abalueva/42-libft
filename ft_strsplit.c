89661a329086989f836b395fb4d0f9fd21c30010
abalueva <evaandbalu@gmail.com>
2019-10-03 19:34:38 -0700

    asdasda

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: abalueva <abalueva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 16:25:31 by abalueva          #+#    #+#             */
-/*   Updated: 2019/10/03 19:30:41 by abalueva         ###   ########.fr       */
+/*   Updated: 2019/10/03 19:34:26 by abalueva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,41 +32,29 @@ static int		ft_find_size(char const *s, char c)
 	return (size);
 }
 
-static char		**find(char const *s, char c, int len)
-{
-	int		i;
-	int		j;
-	int		z;
-	char	**fresh;
-
-	i = 0;
-	j = 0;
-	z = 0;
-	if (!(fresh = malloc(sizeof(char *) * (len + 1))))
-		return (NULL);
-	while (z < len)
-	{
-		while (s[i] == c && s[i] != '\0')
-			++i;
-		j = i;
-		while (s[j] != c && s[j] != '\0')
-			++j;
-		fresh[z] = ft_strsub(s, i, j - i);
-		z++;
-		i = j;
-	}
-	fresh[z] = NULL;
-	return (fresh);
-}
-
 char			**ft_strsplit(char const *s, char c)
 {
-	int		len;
-	char	**fresh;
+	char	**r;
+	int		fl;
+	size_t	i[3];
 
+	fl = 0;
+	i[0] = 0;
+	i[2] = -1;
 	if (!s || !c)
 		return (NULL);
-	len = ft_find_size(s, c);
-	fresh = find(s, c, len);
-	return (fresh);
+	if (!(r = (char **)malloc(sizeof(char *) * (ft_find_size(s, c) + 1))))
+		return (NULL);
+	while (++i[2] < ft_find_size(s, c))
+	{
+		while (s[i[0]] == c && s[i[0]] != '\0')
+			i[0]++;
+		i[1] = i[0];
+		while (s[i[1]] != c && s[i[1]] != '\0')
+			i[1]++;
+		r[i[2]] = ft_strsub(s, i[0], i[1] - i[0]);
+		i[0] = i[1];
+	}
+	r[i[2]] = (NULL);
+	return (r);
 }
